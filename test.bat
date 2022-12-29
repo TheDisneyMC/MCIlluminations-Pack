fe8c211bead5158042fb5d937dbd6e0d224b6a49
KyGuy2002 <kylanjaeger@gmail.com>
2022-12-28 21:52:09 -0800

    Initial Commit

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Zipping...
+"C:\Program Files\7-Zip\7z.exe" a -tzip ".\work\local-pack.zip" ".\pack\*"
+echo Installing To Client...
+copy /Y ".\work\local-pack.zip" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\local-pack.zip"
+echo Done!  Please use F3+T to reload in game.  Remember, server pack takes priority over changes.
