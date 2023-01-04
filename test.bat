341dc593f57717c578e47b55766a1fbe08fdba4b
KyGuy2002 <kylanjaeger@gmail.com>
2023-01-03 17:52:47 -0800

    added sm models, fixed stuff

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Zipping...
-"C:\Program Files\7-Zip\7z.exe" a -tzip ".\work\local-pack.zip" ".\pack\*"
+"C:\Program Files\7-Zip\7z.exe" a -tzip ".\work\local-pack-m.zip" ".\pack\*"
 echo Installing To Client...
-copy /Y ".\work\local-pack.zip" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\local-pack.zip"
+copy /Y ".\work\local-pack-m.zip" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\local-pack-m.zip"
 echo Done!  Please use F3+T to reload in game.  Remember, server pack takes priority over changes.
